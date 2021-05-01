21c84a7d3dc0777c60db1d6266736e5e8f4e5906
Marko Tiikkaja <marko@joh.to>
2021-05-01 21:31:52 +0300

    Pass pg_config --cflags to cmake for building brotli

--- build_brotli.sh
+++ build_brotli.sh
@@ -6,6 +6,7 @@ git clone https://github.com/google/brotli.git
 cd brotli
 mkdir out
 cd out
+export CFLAGS="$(pg_config --cflags)"
 cmake -DCMAKE_BUILD_TYPE=Release -DCMAKE_INSTALL_PREFIX=./brotli_install ..
 cmake --build . --config Release --target install
 mv brotli_install ../../
